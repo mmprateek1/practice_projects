3454e9ed501224e09ce886caea1a0e41a3c19c15
M M PRATEEK <mmprateek2004@gmail.com>
2026-08-21 10:22:33 +0530

    Refactor game logic to handle multiple choices and improve event handling

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -1,7 +1,38 @@
 let userScore = 0;
 let comScore = 0;
 
-const choices = document.querySelector(".choice");
+const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () =>{
+    const options = ["rock","paper","scissors"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+    // rock,paper,scissors
 
+}
+
+const drawGame = ()=>{
+    console.log("game was draw.");
+}
+
+const playGame=(userChoice)=>{
+    console.log("user choice = ",userChoice);
+    // Generate Computer Choice
+    const compChoice = genCompChoice();
+    console.log("comp choice = ",compChoice);
+
+    if(userChoice == compChoice){
+        // Draw Game
+        drawGame();
+    }
+
+}
+
+choices.forEach((choice)=>{
+    console.log(choice);
+    choice.addEventListener("click",()=>{
+        const userChoice= choice.getAttribute("id")
+        playGame(userChoice);
+    })
+})
 
